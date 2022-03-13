4342d2503a72a1bf017bc47d1b26dcbefbfb2062
GitHub Action <action@github.com>
2022-03-13 07:13:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/brighttank/haxeplugin/MyBundle.kt
+++ src/main/kotlin/com/github/brighttank/haxeplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.brighttank.haxeplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/brighttank/haxeplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/brighttank/haxeplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.brighttank.haxeplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.brighttank.haxeplugin.MyBundle
 
 class MyApplicationService {
 
